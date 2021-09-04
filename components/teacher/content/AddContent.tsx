15eec5256887049e5521a383d554a2058af4f49d
cholazzzb <nicsphehehe@gmail.com>
2021-09-04 23:58:22 +0700

    debug

--- components/teacher/content/AddContent.tsx
+++ components/teacher/content/AddContent.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 const AddContent = () => {
+  const router = useRouter();
   const [formData, setFormData] = useState<any>({});
   const [notif, setNotif] = useState<string>("");
 
@@ -27,39 +29,50 @@ const AddContent = () => {
 
     const resData = await res.json();
     if (resData.success) {
+      setNotif("Success!");
       setTimeout(() => {
-        setNotif("Success!");
-      }, 2000);
+        setNotif("");
+      }, 1500);
     }
   };
 
   return (
-    <form onSubmit={createContent}>
-      {notif !== "" ? <div>{notif}</div> : <></>}
-      <div className="flex flex-col p-2">
-        <label htmlFor="topic">Topic</label>
-        <input
-          onChange={handleFormChange}
-          className="p-2 rounded-xl"
-          type="text"
-          placeholder="Topic"
-          name="topic"
-        />
-        <label htmlFor="content">Content</label>
-        <textarea
-          onChange={handleFormChange}
-          className="p-2 rounded-xl"
-          placeholder="Content"
-          name="content"
-        ></textarea>
-        <button
-          className="rounded-xl border-2 border-white p-2 mt-2"
-          type="submit"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
+    <>
+      {notif !== "" ? (
+        <div className="fixed z-50 top-10 left-0 flex w-screen justify-center items-center">
+          <div className="border-2 border-white rounded-full p-4 bg-black text-white bg-opacity-50">
+            Content Added Successfully!
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+      <form onSubmit={createContent}>
+        <div className="flex flex-col p-2">
+          <label htmlFor="topic">Topic</label>
+          <input
+            onChange={handleFormChange}
+            className="p-2 rounded-xl"
+            type="text"
+            placeholder="Topic"
+            name="topic"
+          />
+          <label htmlFor="content">Content</label>
+          <textarea
+            onChange={handleFormChange}
+            className="p-2 rounded-xl"
+            placeholder="Content"
+            name="content"
+          ></textarea>
+          <button
+            className="rounded-xl border-2 border-white p-2 mt-2"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
